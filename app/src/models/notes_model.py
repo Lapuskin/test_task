97349ca4e009cb11e5f0588cbd2fdd649623c322
lapuskin <mikhail.lapkovskiy.2004@gmail.com>
2024-02-18 15:20:55 +0300

    add models

---
+++ app/src/models/notes_model.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, Column, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    email = Column(String(50))
+
+    notes = relationship('Note', back_populates='user')
+
+class Note(Base):
+    __tablename__ = 'notes'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100))
+    content = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    user = relationship('User', back_populates='notes')
